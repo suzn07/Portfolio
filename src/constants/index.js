d63f085b7e30d61965bda43795f28a981be634b1
suzn077 <baralisujan101@gmail.com>
2024-12-24 20:24:50 +0545

    portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,85 @@
+import project1 from "../assets/projects/tutuhaoyi.jpeg";
+import project2 from "../assets/projects/pickahouse.jpeg";
+import project4 from "../assets/projects/tcs.jpeg";
+import project3 from "../assets/projects/project-3.jpg";
+import project5 from "../assets/projects/ec.jpg";
+
+export const HERO_CONTENT = `I am a passionate Front-End developer with a knack for crafting robust and scalable web development. With 3 years of hands-on experience, I have honed my skills in front-end technologies like React, Next.js and Wordpress, as well as back-end technologies like Node.js, MySQL and Graphql. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile Front-End developer with a passion for creating efficient and user-friendly web development. With 3 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, Wordpress, MySQL and Graphql. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - 2023",
+    role: "Interin as Front-End Developer",
+    company: "Bootwal Research and Development Pvt. Ltd.",
+    description: `An intern working on web applications using ReactJS to build websites and MongoDB to store and manage data.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2024",
+    role: "Frontend Developer",
+    company: "Pcsoftnepal",
+    description: `Designed and developed user interfaces for web applications using Next.js, React and Wordpress also Developed and maintained web applications using JavaScript, React.js, and Node.js.. Implemented responsive designs and optimized frontend performance.`,
+    technologies: [
+      "HTML",
+      "CSS",
+      "Figma",
+      "Nextjs",
+      "React.js",
+      "Wordpress",
+      "Graphql",
+    ],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Article Website",
+    image: project1,
+    description:
+      "A platform for sharing in-depth news analysis and editorials. A resource for enthusiasts seeking the latest updates on chinese artworks.",
+    technologies: ["HTML", "CSS", "SASS", "React", "Wordpress", "Graphql"],
+  },
+  {
+    title: "Real estate Website",
+    image: project2,
+    description:
+      "A unique marketplace for real estate properties. Free listings for buying, selling, renting & sharing houses & apartments.",
+    technologies: [
+      "CSS",
+      "SASS",
+      "Bootstrap",
+      "Nextjs",
+      "Wordpress",
+      "Graphql",
+    ],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "TCS Telecom is the proud communications provider to thousands of Anglo-Israelis. We're also one of Israel's fastest-growing telecommunications companies.",
+    technologies: ["HTML", "CSS", "SASS", "Reactjs", "Wordpress", "Graphql"],
+  },
+  {
+    title: "E-Commerce Website",
+    image: project5,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "Wordpress", "Php"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "Reactjs", "Tailwind"],
+  },
+];
+
+export const CONTACT = {
+  address: "Tilottama-5, Rupandehi, Nepal",
+  phoneNo: "+9779821511598",
+  email: "baralisujan101@gmail.com",
+};
